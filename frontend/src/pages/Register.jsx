3e4b483c3684b1ca990e116ce91e0506e3310c4f
anna karolyne <129319846+AnnaKArolynee@users.noreply.github.com>
2024-06-19 23:51:38 -0300

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { useNavigate } from 'react-router-dom'; // Correta importação de useNavigate
+import axios from 'axios';
 import './Register.css'
 const Register = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const history = useNavigate();
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // Lógica de registro
-    history.push('/dashboard');
+   
+    try {
+      const response = await axios.post('http://localhost:3000/register', { email, password });
+      console.log('Resposta do servidor:', response.data);
+      alert('Registro bem-sucedido!');
+     // navigate('/login'); // Usar navigate ao invés de history.push
+    } catch (error) {
+      console.error('Erro ao fazer cadastro:', error);
+      if (error.response.status === 404) {
+        alert('Erro ao fazer o registro: Recurso não encontrado. Por favor, contate o suporte.');
+      } else {
+        alert('Erro ao fazer o registro. Por favor, tente novamente.');
+      }
+    }
   };
 
   return (
