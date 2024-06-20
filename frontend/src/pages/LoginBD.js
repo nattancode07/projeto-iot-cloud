1c23f1626c1def98f1d20fba6c610a5091f76e8f
anna karolyne <129319846+AnnaKArolynee@users.noreply.github.com>
2024-06-19 23:51:33 -0300

    Update LoginBD.js

--- frontend/src/pages/LoginBD.js
+++ frontend/src/pages/LoginBD.js
@@ -1,47 +1,31 @@
 const express = require('express');
 const mysql = require('mysql');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
-
+app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
 
-// Configurar a conexão com o banco de dados
 const db = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'an@2208',
-  database: 'adm'
+  host: "127.0.0.1",
+  use: "root",
+  password: "an@2208",
+  database: "adm"
 });
 
-db.connect(err => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
-  } else {
-    console.log('Conectado ao banco de dados');
-  }
-});
+app.post('/login', (req, res) =>{
+    const sql = "SELECT * FROM usuario WHERE email = ? AND senha = ?";
+    const values = [
+      req.body.email,
+      req.body.password
+    ]
+    db.query(sql, [values], (err, data) => {
+      if(err) return res.json("Falha no Login");
+      return res.json(data);
+    })
+})
 
-// Rota de login
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
+app.listen(8081, () => {
+  console.log("Listening...");
+})
 
-  const query = 'SELECT * FROM usuario WHERE email = ? AND senha = ?';
-  db.query(query, [email, password], (err, results) => {
-    if (err) {
-      console.error('Erro ao executar a consulta:', err);
-      res.status(500).send('Erro ao conectar ao banco de dados');
-    } else if (results.length > 0) {
-      res.send({ message: 'Seja bem-vindo' });
-    } else {
-      res.status(401).send({ message: 'Usuário ou senha incorretos' });
-    }
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
